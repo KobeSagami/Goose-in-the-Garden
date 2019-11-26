39884be33200a9eff4881aa9b278d3e731d02eee
tristan <tristanlinn25@gmail.com>
2019-11-26 09:45:27 -0700

    Enemy AI

--- src/sample/Enemy.java
+++ src/sample/Enemy.java
@@ -5,8 +5,9 @@ import javafx.scene.layout.Pane;
 
 public class Enemy extends SpriteBase {
 
-    public Enemy(Pane layer, Image image, double x, double y, double dx, double dy, String mapName ) {
-        super(layer, image, x, y,  dx, dy, mapName);
+    public Enemy(Pane layer, Image image, double x, double y, double dx, double dy) {
+        super(layer, image, x, y,  dx, dy);
+
     }
 
     @Override
@@ -15,6 +16,58 @@ public class Enemy extends SpriteBase {
         if( Double.compare( getY(), Settings.SCENE_HEIGHT) > 0) {
             setRemovable(true);
         }
+
+
+    }
+
+        public void AI(boolean canmoveleft, boolean canmoveright, boolean canmoveup, boolean canmovedown, double Playerx, double Playery){
+            double toworkonx = Playerx;
+            double toworkony = Playery;
+            boolean currentmovement = false;
+
+            if(dy > 0){
+                currentmovement = canmovedown;
+            }
+            if(dy < 0){
+                currentmovement = canmoveup;
+            }
+            if(dx < 0){
+                currentmovement = canmoveleft;
+            }
+            if(dx > 0){
+                currentmovement = canmoveright;
+            }
+
+            if(!currentmovement){
+                dy = 0;
+                dx = 0;
+                if(toworkony < y && canmoveup){
+                    dy = -5;
+                }
+                else if(toworkony > y && canmovedown){
+                    dy = 5;
+                }
+                else if (toworkonx < x && canmoveleft){
+                    dx = -5;
+                }
+                else if(toworkonx > x && canmoveright){
+                    dx = 5;
+                }
+                else{
+                    if(canmoveup){
+                        dy = -5;
+                    }
+                    else if(canmovedown){
+                        dy = 5;
+                    }
+                    else if (canmoveright){
+                        dx = 5;
+                    }
+                    else if (canmoveleft){
+                        dx = -5;
+                    }
+                }
+            }
 
 
     }
